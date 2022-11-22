c49fe5e82291122c935870ed4157fa7ef8a231d9
Bruno Santana Lopes <brunolopesg@gmail.com>
2022-11-22 20:21:58 -0300

    First Commit

---
+++ Monitoria/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bekas
+ *
+ */
+module Monitoria {
+}
---
+++ Monitoria/src/tech/ada/monitoria/Main.java
@@ -0,0 +1,12 @@
+package tech.ada.monitoria;
+
+import tech.ada.monitoria.controller.AlunoController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AlunoController controller = new AlunoController();
+		controller.menu();
+	}
+
+}
